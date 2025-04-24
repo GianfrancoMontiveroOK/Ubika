9251bf07dc17e15cc618c4645a037396e823a341
Reikisr <montiverogianfranco2709@gmail.com>
2025-04-24 10:08:02 -0300

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // Define la URL base según el entorno
 const baseURL =
   process.env.NODE_ENV === "production"
-    ? "https://www.api.vitalveg.shop/api" // URL para producción
+    ? "https://server-14xb.onrender.com/api" // URL para producción
     : "http://localhost:4000/api"; // URL para desarrollo
 
 const instance = axios.create({
